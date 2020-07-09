95c190553369e8186cc102764fe41fa8ea8ebe95
LDongGyu <ehdrbchd@naver.com>
2020-07-09 23:03:47 +0900

    style: floatingActionButton 추가

--- app/src/main/java/com/example/keyboard/MainActivity.kt
+++ app/src/main/java/com/example/keyboard/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.keyboard
 
+import android.animation.ObjectAnimator
 import android.content.Context
+import android.content.Intent
 import android.graphics.drawable.Icon
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -15,6 +17,7 @@ import com.example.keyboard.fragment.AllFragment
 import com.example.keyboard.fragment.CategoryFragment
 import com.example.keyboard.fragment.SettingFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -23,6 +26,7 @@ class MainActivity : AppCompatActivity() {
     private var allFragment = AllFragment()
     private var categoryFragment = CategoryFragment()
     private var settingFragment = SettingFragment()
+    private var isOpen = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +36,9 @@ class MainActivity : AppCompatActivity() {
         transaction.replace(R.id.mainFragment, allFragment).commit()
 
         bottomNavigationView.setOnNavigationItemSelectedListener(clickListener)
+        mainFab.setOnClickListener(mainFabClickListener)
+        itemFab.setOnClickListener(itemFabClickListener)
+        categoryFab.setOnClickListener(categoryFabClickListener)
     }
 
     val clickListener: BottomNavigationView.OnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener {
@@ -42,6 +49,27 @@ class MainActivity : AppCompatActivity() {
             else -> newTransaction.replace(R.id.mainFragment, settingFragment).commit()
         }
         return@OnNavigationItemSelectedListener true
+    }
+
+    val mainFabClickListener: View.OnClickListener = View.OnClickListener {
+        if(!isOpen) { // fab이 눌리지 않았다면
+            ObjectAnimator.ofFloat(itemFab, "translationY", -400f).apply { start() }
+            ObjectAnimator.ofFloat(categoryFab, "translationY", -200f).apply { start() }
+            isOpen = true
+        }
+        else{ // fab이 눌렸다면
+            ObjectAnimator.ofFloat(itemFab, "translationY", 0f).apply { start() }
+            ObjectAnimator.ofFloat(categoryFab, "translationY", 0f).apply { start() }
+            isOpen = false
+        }
+    }
+
+    val itemFabClickListener: View.OnClickListener = View.OnClickListener {
+        startActivity(Intent(this, ItemCreateActivity::class.java))
+    }
+
+    val categoryFabClickListener: View.OnClickListener = View.OnClickListener {
+        startActivity(Intent(this, CategoryCreateActivity::class.java))
     }
 }
 
