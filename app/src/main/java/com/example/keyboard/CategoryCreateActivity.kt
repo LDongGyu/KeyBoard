806701fb5ecda6e51e324bc2ae69de449c5a543f
LDongGyu <ehdrbchd@naver.com>
2020-07-11 16:18:06 +0900

    feat: CategoryCreateActivity 수정 버튼 기능 구현

--- app/src/main/java/com/example/keyboard/CategoryCreateActivity.kt
+++ app/src/main/java/com/example/keyboard/CategoryCreateActivity.kt
@@ -2,11 +2,36 @@ package com.example.keyboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_category_create.*
 
 class CategoryCreateActivity : AppCompatActivity() {
+
+    private var isCanChange = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category_create)
+
+        editBtn.setOnClickListener(editBtnClickListener)
+
+    }
+
+    private val editBtnClickListener: View.OnClickListener = View.OnClickListener {
+        if(!isCanChange){
+            isCanChange = true
+            editBtn.text = "수정 OFF"
+        }
+        else{
+            isCanChange = false
+            editBtn.text = "수정 ON"
+        }
+        toggleEditText(isCanChange)
+
+    }
+
+    private fun toggleEditText(flag: Boolean){
+        titleEditTxt.isEnabled = flag
+        etcEditTxt.isEnabled = flag
     }
 }
