544b59716e728b2ab912875fec7268561586fde8
LDongGyu <ehdrbchd@naver.com>
2020-07-26 16:24:25 +0900

    feat: item create 할 때 category read API 연동해서 선택 추가
    spinner에서 user가 가진 category DB에서 읽어와서 선택하게 해줌

--- app/src/main/java/com/example/keyboard/feature/Item/ItemCreateActivity.kt
+++ app/src/main/java/com/example/keyboard/feature/Item/ItemCreateActivity.kt
@@ -6,29 +6,51 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.example.keyboard.R
 import com.example.keyboard.api.DBServiceImpl
 import com.example.keyboard.data.GetCategory
 import com.example.keyboard.data.GetStatus
+import com.example.keyboard.feature.CategoryList.CategoryListItem
 import com.example.keyboard.feature.KeyList.KeyItem
 import com.example.keyboard.feature.MainActivity
 import com.example.keyboard.feature.Singleton.UserInfo
 import kotlinx.android.synthetic.main.activity_item_create.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import retrofit2.Call
 
 class ItemCreateActivity : AppCompatActivity() {
 
-    private lateinit var categoryList: MutableList<GetCategory>
+    private lateinit var categoryList: List<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_item_create)
+        runBlocking {
+            categoryList = initCategoryList()
+        }
+        var adapter = ArrayAdapter<String>(applicationContext,android.R.layout.simple_spinner_dropdown_item,categoryList)
+        categorySpinner.adapter = adapter
         createBtn.setOnClickListener(itemCreateBtnClickListener)
+    }
+
+    private suspend fun initCategoryList():List<String>{
+        lateinit var temp:List<GetCategory>
+        var data = mutableListOf<String>()
+
+        val call: Call<List<GetCategory>> = DBServiceImpl.service.getCategory(UserInfo.id)
+        var job = CoroutineScope(Dispatchers.IO).launch {
+            var result = call.execute()
+            temp = result.body()!!
+        }
+        job.join()
+
+        for(category in temp){
+            data.add(category.category)
+        }
+
+        return data
     }
 
     private val itemCreateBtnClickListener: View.OnClickListener = View.OnClickListener {
